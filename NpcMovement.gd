a0b365b52e3ee84550f0c28a5ab3e1bd3f327568
RedhawkPhyre <shiraphyre@gmail.com>
2024-11-02 17:30:10 -0600

    NPC movement

---
+++ NpcMovement.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@onready var animation_player = $AnimationPlayer
+
+func _ready() -> void:
+	# Play the move animation when the NPC is ready
+	animation_player.play("NpcEnter")
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):  # Replace with your specific action
+		animation_player.play("NpcEnter")
+		
+func _process(delta: float) -> void:
+	# Additional logic can be added here if needed
+	pass
