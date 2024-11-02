b1ff75b55cb23f100962f20e83581876e2ba2dc6
RedhawkPhyre <shiraphyre@gmail.com>
2024-11-02 17:37:08 -0600

    NPC Resource file

---
+++ npcResource.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name NpcResource
+
+@export var npc_name: String = ""
+@export_multiline var npc_request_dialogue: String = ""
+@export var content_with_item: bool = false
+
+@export var npc_end_dialogue: Array = []
+@export var npc_item_response: Array = []
+@export var npc_yelp: Array = []
