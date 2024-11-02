0b2fe5ad8faa84d255e7b618cc428f52876fe941
RedhawkPhyre <shiraphyre@gmail.com>
2024-11-01 21:48:05 -0600

    initializing item script

---
+++ itemScript.gd
@@ -0,0 +1,18 @@
+extends Node
+
+#Item Characterists
+var item_name: String = ""
+var item_real_description: String = ""
+var item_initial_description: String = ""
+var item_is_cursed: bool = false
+var item_available: bool = true
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
