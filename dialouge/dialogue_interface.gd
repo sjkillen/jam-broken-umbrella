a69c7c234bee56fb805464bb7e9d344b6d187bb3
Wren Durbano <wren.durbano@gmail.com>
2024-11-02 15:44:59 -0600

    fix

--- dialouge/dialogue_interface.gd
+++ dialouge/dialogue_interface.gd
@@ -6,12 +6,10 @@ const CHARS_PER_SECOND = 80
 
 var display_time: float = 0
 
-func _ready():
-	display_text("Lorem ipsum odor amet, consectetuer adipiscing elit. Potenti consectetur ligula enim malesuada turpis maximus ipsum. Libero ad semper dignissim tincidunt pretium urna elit orci. In mollis donec, vel inceptos donec volutpat ultricies efficitur. Felis tincidunt taciti lectus hac accumsan. Fusce leo curae sociosqu parturient faucibus donec torquent convallis placerat. Tortor ridiculus potenti eget ullamcorper per vel lacus posuere natoque. Curabitur curae conubia suspendisse ligula fusce dictum.");
-
 func _process(delta):
 	display_time += delta
 	text_label.visible_characters = floori(display_time * CHARS_PER_SECOND)
+	visible = text_label.text.length() > 0
 
 func display_text(text: String):
 	text_label.text = text
